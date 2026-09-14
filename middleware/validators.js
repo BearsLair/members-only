c5cf5f5d7157ad440ee4b5d572995790ac703b28
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-09-14 00:57:16 -0700

    fix: isEmpty message now displays

--- middleware/validators.js
+++ middleware/validators.js
@@ -8,22 +8,26 @@ exports.validators = [
     .withMessage("First name cannot be empty.")
     .isAlpha()
     .withMessage("First name must contain only alphabet characters.")
-    .isLength("First name must be less than 30 characters"),
+    .isLength({ max: 30 })
+    .withMessage("First name must be less than 30 characters"),
   body("lastname")
     .trim()
     .notEmpty()
     .withMessage("Last name cannot be empty.")
     .isAlpha()
     .withMessage("Last name must contain only alphabet characters.")
-    .isLength("Last name must be less than 30 characters"),
+    .isLength({ max: 30 })
+    .withMessage("Last name must be less than 30 characters"),
   body("username")
     .trim()
     .notEmpty()
     .withMessage("Username cannot be empty.")
-    .isLength("First name must be less than 30 characters"),
+    .isLength({ max: 30 })
+    .withMessage("First name must be less than 30 characters"),
   body("password")
     .trim()
     .notEmpty()
     .withMessage("Password cannot be empty.")
-    .isLength("Password must be less than 30 characters"),
+    .isLength({ max: 30 })
+    .withMessage("Password must be less than 30 characters"),
 ];
