bee56dc130e4355e8f6f4933a1bf443e489ed255
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-09-14 00:11:08 -0700

    added validation to sign-up post

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,3 +1,5 @@
+const { validationResult } = require("express-validator");
+
 async function getHomePage(req, res) {
   try {
     res.render("home");
@@ -8,7 +10,7 @@ async function getHomePage(req, res) {
 
 async function getSignUp(req, res) {
   try {
-    res.render("sign-up");
+    res.render("sign-up", { formData: {} });
   } catch (error) {
     console.error("Error retrieving sign-up page: ", error);
   }
@@ -16,6 +18,17 @@ async function getSignUp(req, res) {
 
 async function postSignUp(req, res) {
   try {
+    //retrieve validation errors from request
+    const errors = validationResult(req);
+
+    // Stop execution and display errors if errors found
+    if (!error.isEmpty()) {
+      return res.status(400).render("sign-up", {
+        errors: errors.array(), // Converts errors to array for iteration
+        formData: req.body, // Passes back entered data so user doesn't re-enter inputs
+      });
+    }
+
     console.log(req.body);
     res.redirect("/");
   } catch (error) {
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,11 +1,12 @@
 const { Router } = require("express");
 const indexRouter = Router();
 const membersController = require("../controllers/membersController");
+const validators = require("../middleware/validators");
 
 indexRouter.get("/", membersController.getHomePage);
 
 indexRouter.get("/sign-up", membersController.getSignUp);
 
-indexRouter.post("/sign-up", membersController.postSignUp);
+indexRouter.post("/sign-up", validators, membersController.postSignUp);
 
 module.exports = indexRouter;
