dd3eed11bca327de8baa18e20b3dc56ff375e4f2
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-09-14 17:24:35 -0700

    added get route to code page

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -22,8 +22,6 @@ async function postSignUp(req, res) {
     //retrieve validation errors from request
     const errors = validationResult(req);
 
-    console.log("errors: ", errors);
-
     // Stop execution and display errors if errors found
     if (!errors.isEmpty()) {
       return res.status(400).render("sign-up", {
@@ -43,8 +41,29 @@ async function postSignUp(req, res) {
   }
 }
 
+async function getCodePage(req, res) {
+  try {
+    res.render("code");
+  } catch (error) {
+    console.error("Error rendering code page: ", error);
+  }
+}
+
+async function postCodePage(req, res) {
+  const errors = validationResult(req);
+
+  try {
+    // No errors? Continue.
+    res.redirect("/");
+  } catch (error) {
+    console.error("Error posting code for verification: ", error);
+  }
+}
+
 module.exports = {
   getHomePage,
   getSignUp,
   postSignUp,
+  getCodePage,
+  postCodePage,
 };
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -10,4 +10,8 @@ indexRouter.get("/sign-up", membersController.getSignUp);
 
 indexRouter.post("/sign-up", validators, membersController.postSignUp);
 
+indexRouter.get("/code", validators, membersController.getCodePage);
+
+indexRouter.post("/code", membersController.postCodePage);
+
 module.exports = indexRouter;
