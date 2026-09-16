ca165b479d19620f075f0b213175a258e2a9bb7e
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-09-16 01:59:09 -0700

    Feat: Users can log in

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const session = require("express-session");
+const passport = require("passport");
+const pool = require("./db/pool");
+
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
 
@@ -9,6 +13,32 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
+
+// Note to self: Authentication middleware must come before routes
+// Connect postgresql db table to browser cookies
+// Note to self: saveUninitialized does NOT go into sessions store
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      pool: pool,
+      createTableIfMissing: true,
+    }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  }),
+);
+
+require("./config/passport");
+
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  next();
+});
+
 app.use("/", indexRouter);
 
 const PORT = process.env.PORT || 3000;
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,6 +4,7 @@ const membersController = require("../controllers/membersController");
 // validators must be destructured
 const { validators } = require("../validators/validators");
 const { codeValidator } = require("../validators/codevalidator");
+const passport = require("passport");
 
 indexRouter.get("/", membersController.getHomePage);
 
@@ -14,5 +15,15 @@ indexRouter.post("/sign-up", validators, membersController.postSignUp);
 indexRouter.get("/code", validators, membersController.getCodePage);
 
 indexRouter.post("/code", codeValidator, membersController.postCodePage);
+
+indexRouter.get("/log-in", membersController.getLoginPage);
+
+indexRouter.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/log-in",
+  }),
+);
 
 module.exports = indexRouter;
