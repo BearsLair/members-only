a0e07effffa7cebee5f3695cbea37d1798f7e6dd
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2026-09-17 17:46:49 -0700

    made deserialize SQL more secure

--- config/passport.js
+++ config/passport.js
@@ -35,9 +35,10 @@ passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser(async (id, done) => {
   try {
-    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
-      id,
-    ]);
+    const { rows } = await pool.query(
+      "SELECT username, member FROM users INNER JOIN userinfo ON userinfo.usersid = users.id WHERE usersid = $1",
+      [id],
+    );
     const user = rows[0];
     done(null, user);
   } catch (err) {
